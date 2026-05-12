242c289a2a578dcd5f1f770e65f6504aeefc432f
rubyannSTAYERY <ruby.grambauer@stayery.com>
2026-05-12 15:02:33 +0200

    File structure setup and added dependencies

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from overbooking-analyse!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,3 @@
+#---------------------------------------
+# This file contains helper functions for the project
+#---------------------------------------
